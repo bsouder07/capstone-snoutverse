5961988a3bd596c5633801af64d22f134fe6bc6f
cedfcampbell <cedrik.campbell.dev@gmail.com>
2023-12-26 16:23:25 -0500

    Completed header bar feature

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,31 +1,36 @@
 import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./Header.css";
+import useAuth from "../../hooks/useAuth";
+import useAuthProvider from "../../providers/AuthProvider/useAuthProvider";
 
 function Header() {
 
-  
+  const { handleSignOut } = useAuth();
+  const { state } = useAuthProvider();
+
+  const isLoggedIn = state.isAuthenticated;
 
   return (
     <Navbar bg="primary" expand="lg" fixed="top">
       <Container>
-        <Navbar.Brand as={Link} to="/" id="title">
+        <Navbar.Brand as={Link} to={isLoggedIn ? "/dashboard" : "/"} id="title">
           SnoutVerse
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="top-nav" />
         <Navbar.Collapse id="top-nav" className="justify-content-end">
           <Nav>
             {isLoggedIn ? (
-              <Nav.Link>Log Out</Nav.Link>
+              <Button onClick={handleSignOut}>Log Out</Button>
             ) : (
               <>
-                <Nav.Link as={Link} to="/register">
+                <Button className="m-2" variant="dark" as={Link} to="/register">
                   Sign Up
-                </Nav.Link>
-                <Nav.Link as={Link} to="/login">
+                </Button>
+                <Button className="m-2" variant="dark" as={Link} to="/login">
                   Sign In
-                </Nav.Link>
+                </Button>
               </>
             )}
           </Nav>
