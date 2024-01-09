838f70d20ae556952b7f5744101f6ace9a9af0eb
Tim Quattrochi <timq82@gmail.com>
2024-01-09 18:49:05 -0500

    fixed empty broken return

--- client/src/pages/AdminDashboardPage.jsx
+++ client/src/pages/AdminDashboardPage.jsx
@@ -2,25 +2,17 @@ import React, { useEffect } from "react";
 import { Container } from "react-bootstrap";
 import { getAdminProtectedResource } from "../services/api.services";
 
-
-
 function AdminDashboardPage() {
-
-  useEffect(()=>{
-     getAdminProtectedResource()
-     .then((response) => console.log(response)) 
-
-  },[])
-  return 
+  useEffect(() => {
+    getAdminProtectedResource().then((response) =>
+      console.log(response)
+    );
+  }, []);
+  return (
     <Container>
-      
-      
-       <h1>DashboardPage </h1>
-    
-    
-    
+      <h1>DashboardPage </h1>
     </Container>
-  
+  );
 }
 
-export default AdminDashboardPage
+export default AdminDashboardPage;
