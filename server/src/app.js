8231527d2407b59dfb4284886b2ac2dc2aff30a7
Tim Quattrochi <timq82@gmail.com>
2024-01-09 19:25:18 -0500

    tweaked path to server static files from express

--- server/src/app.js
+++ server/src/app.js
@@ -31,7 +31,7 @@ app.use(keys.api_url, router);
 app.use(keys.api_url, router);
 
 if (keys.node_env === "production") {
-  app.use(express.static(path.join(__dirname, "../client/dist")));
+  app.use(express.static(path.join(__dirname, "./client/dist")));
   app.all("*", (req, res, next) => {
     res.sendFile(
       path.resolve(__dirname, "../client/dist/index.html")
